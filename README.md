3b51f002bfcbc837c9ae68ed10ee9defbc99d1e7
Jonathan Provost <jprovost@internap.com>
2016-05-19 10:43:27 -0400

    Use PBR to deploy the application

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 python-hostsresolver
 ====================
+[![Build Status](https://travis-ci.org/internap/python-hostsresolver.svg?branch=master)](https://travis-ci.org/internap/python-hostsresolver)
+[![PyPI version](https://badge.fury.io/py/python-hostsresolver.svg)](http://badge.fury.io/py/python-hostsresolver)
 
 Translate ephemeral virtual machines names into IP addresses for python
 projects without requiring any modification to the libraries or any
--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+from setuptools import setup
 
 setup(
-    name='python-hostsresover',
-    packages=['hostsresolver'],
-    url='https://github.com/internap/python-hostsresover',
-    license='Apache License, Version 2.0',
-    author='Internap Hosting',
-    author_email='opensource@internap.com',
-    description=
-        'Translate ephemeral virtual machines names into IP addresses for python projects without requiring '
-        'any modification to the libraries or any external domain name server.',
-    install_requires=[
-        'python-vagrant',
-    ]
+    setup_requires=['pbr'],
+    pbr=True,
 )
